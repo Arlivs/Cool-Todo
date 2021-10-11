a543725b8af2e87c969f1de57310f66536222559
Arlivs <ilyayev00@bk.ru>
2021-10-11 21:22:38 +0300

    Finish

--- app.js
+++ app.js
@@ -1,55 +1,66 @@
-const todoBoxCancels = document.querySelectorAll(".todo-box-cancel"),
-todoMenuItem = document.querySelectorAll(".todo-menu__item"),
+const todoMenuItem = document.querySelectorAll(".todo-menu__item"),
+todoInput = document.querySelector(".todo-create__input"),
 todoOpenBtn = document.querySelector(".todo-create__btn"),
-todoChecks = document.querySelectorAll(".todo-checkbox"),
+todoCompletes = document.querySelector(".todo-completed"),
+todoAddBtn = document.querySelector(".todo-add__btn"),
+todoDeletes = document.querySelector(".todo-deleted"),
 todoMenu = document.querySelector(".todo-create"),
-todoBox = document.querySelector(".todo-box"),
-todoList = document.querySelector(".todo-list"),
-todoAddBtn = document.querySelector(".todo-add__btn")
+todoBoxs = document.querySelectorAll(".todo-box"),
+todoList = document.querySelector(".todo-list")
+let taskList = []
 
 const completeTodo = target => {
     target.remove()
-    document.querySelector(".todo-completed").append(target)
+    todoCompletes.append(target)
     target.classList.add("completed")
+    sessionStorage.setItem("todoComplete", todoCompletes.innerHTML)
 }
 
 const deleteTodo = target => {
     target.remove()
-    document.querySelector(".todo-deleted").append(target)
+    todoDeletes.append(target)
     target.classList.add("deleted")
+    sessionStorage.setItem("todoDelete", todoDeletes.innerHTML)
 }
 
+const todoEvents = () => {
+    document.querySelectorAll(".todo-checkbox").forEach(el => {
+        el.addEventListener("click", e => {
+            // e.target.parentNode.classList.toggle("complete")
+            setTimeout(completeTodo(e.target.parentNode), 3000)
+        })
+    })
+    document.querySelectorAll(".todo-box-cancel").forEach(el => {
+        el.addEventListener("click", e => deleteTodo(e.target.parentNode))
+    })
+}
 const createTodo = () => {
-    const newTodo = document.createElement("div")
-    newTodo.classList.add("todo-box")
-
-    const todoCheck = document.createElement("input")
-    todoCheck.setAttribute("type", "radio")
-    todoCheck.classList.add("todo-checkbox")
-
-    const todoInner = document.createElement("div")
-    todoInner.classList.add("todo-inner")
-    todoInner.innerHTML = document.querySelector(".todo-create__input").value
-
-    const todoCancel = document.createElement("button")
-    todoCancel.classList.add("todo-box-cancel")
-
-    todoCheck.addEventListener("click", e => e.target.parentNode.classList.toggle("complete"))
-    todoCancel.addEventListener("click", e => deleteTodo(e.target.parentNode))
-    todoCheck.addEventListener("focus", e => completeTodo(e.target.parentNode))
-
-    newTodo.append(todoCheck, todoInner, todoCancel)
-    todoList.append(newTodo)
-    // sessionStorage.setItem("todo", todoList.innerHTML)
+    const newTodo = { todo: todoInput.value }
+    if (newTodo.todo.trim() == "") return
+    taskList.push(newTodo)
+    sessionStorage.setItem("todos", JSON.stringify(taskList))
+    addTodo()
 }
 
+const addTodo = () => {
+    let todoInner = ""
+    taskList.forEach(el => {
+        todoInner += `
+        <div class="todo-box">
+            <input type="radio" class="todo-checkbox">
+            <div class="todo-inner">${el.todo}</div>
+            <button class="todo-box-cancel"></button>
+        </div>`
+        todoList.innerHTML = todoInner
+    })    
+    todoEvents()
+    todoInput.value = ""
+}
 todoAddBtn.addEventListener("click", createTodo)
 
 todoOpenBtn.addEventListener("click", () => todoMenu.classList.toggle("active"))
 
-todoMenuItem.forEach(el => {
-    el.addEventListener("click", e => e.preventDefault())
-})
+todoMenuItem.forEach(el => el.addEventListener("click", e => e.preventDefault()))
 
 function tabFunc() {
     const menuBtns = document.querySelectorAll(".todo-menu__item")
@@ -65,4 +76,24 @@ function tabFunc() {
     })
 }tabFunc()
 
-// todoList.innerHTML = sessionStorage.getItem("todo")
+todoCompletes.innerHTML = sessionStorage.getItem("todoComplete")
+todoDeletes.innerHTML = sessionStorage.getItem("todoDelete")
+if (sessionStorage.getItem("todos")) {
+    taskList = JSON.parse(sessionStorage.getItem("todos"))
+    Array.from(todoCompletes.children).forEach( el => {
+        taskList.forEach(item => {
+            if (item.todo === el.querySelector(".todo-inner").innerHTML) {
+                taskList.splice(taskList.indexOf(item), 1)
+            }
+        })
+    })
+    Array.from(todoDeletes.children).forEach( el => {
+        taskList.forEach(item => {
+            if (item.todo === el.querySelector(".todo-inner").innerHTML) {
+                taskList.splice(taskList.indexOf(item), 1)
+            }
+        })
+    })
+    sessionStorage.setItem("todos", JSON.stringify(taskList))
+    addTodo()   
+}
